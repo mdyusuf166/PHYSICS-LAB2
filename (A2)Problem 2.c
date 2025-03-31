b7070a4233ec49a3a63ef2f8304cf7cd9be47833
MD Yousuf <yousuf381256@gmail.com>
2025-03-31 15:36:34 +0600

    Update (A2)Problem 2.c

--- (A2)Problem 2.c
+++ (A2)Problem 2.c
@@ -3,8 +3,8 @@
 
 
 int main() {
-    double theta = 45.0;  // Angle in degrees
-    double sin_theta = sin(theta * M_PI / 180.0); // Convert to radians and compute sine
+    double theta = 45.0;  
+    double sin_theta = sin(theta * M_PI / 180.0);
     double ratio = 1.0 / sin_theta;
 
     printf("The ratio of slit width to wavelength is: %.5f\n", ratio);
